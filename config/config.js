38f61f6b6af3fcbe291f517d69f865868e0e8d23
Kishore <rama.kishore@g11n.in>
2016-05-19 11:19:33 +0530

    updated config and express js files

--- config/config.js
+++ config/config.js
@@ -1,8 +1,10 @@
 const mongodbUrl = "mongodb://localhost:27017/MEAN";
 const mongodbPort = 3009;
+const path = require('path');
 
 
 exports.config = {
   mongodbUrl:mongodbUrl,
-  mongodbPort:mongodbPort
+  mongodbPort:mongodbPort,
+  root:path.join(__dirname , '..')
 }
--- config/express.js
+++ config/express.js
@@ -3,12 +3,14 @@
 const express = require('express');
 const path = require('path');
 const ejs = require('ejs');
+const config = require('./config').config;
 
 
 module.exports = function(app) {
 
 app.set('view engine' , 'ejs');
-app.set('views', '../app/views/');
+app.set('views', path.join(config.root , 'app' , 'views'));
+console.log(path.join(__dirname , '..'));
 // path.join(__dirname, 'app/views')
 
 }
